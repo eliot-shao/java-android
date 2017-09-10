0a9c14b162737ac03b2f9d6379c21e465d9f2da4
eliot shao <805724100@qq.com>
2017-09-10 10:35:20 +0800

    Create function1.java

---
+++ helloworld/function1.java
@@ -0,0 +1,17 @@
+package helloworld;
+
+public class function1 {
+	
+	String temp;
+	{System.out.println("this is function1 var area");}
+	/*构造函数*/
+	function1() {
+		
+		System.out.println("this is function1 Constructor ");
+	}
+	public void Test()
+	{
+		
+		System.out.println("this is class function1 Test function "+temp);
+	}
+}
